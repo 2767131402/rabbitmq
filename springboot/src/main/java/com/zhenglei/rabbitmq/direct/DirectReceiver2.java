e165d08d02aa7b284abf8673b541858a6e115ef8
zhenglei <zhenglei@bbdservice.com>
2020-09-02 13:05:21 +0800

    springboot

---
+++ springboot/src/main/java/com/zhenglei/rabbitmq/direct/DirectReceiver2.java
@@ -0,0 +1,34 @@
+package com.zhenglei.rabbitmq.direct;
+
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Component
+@RabbitListener(queues = "TestDirectQueue")//监听的队列名称 TestDirectQueue
+public class DirectReceiver2 {
+ 
+    @RabbitHandler
+    public void process(Map testMessage, Channel channel, Message message) throws Exception {
+        System.out.println("DirectReceiver消费者2收到消息  : " + testMessage.toString());
+        try {
+            Thread.sleep(2000);
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        } catch (IOException e) {
+            /**
+             * 有异常就绝收消息
+             * basicNack(long deliveryTag, boolean multiple, boolean requeue)
+             * requeue:true为将消息重返当前消息队列,还可以重新发送给消费者;
+             *         false:将消息丢弃
+             */
+            channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, false);
+            e.printStackTrace();
+        }
+    }
+ 
+}
